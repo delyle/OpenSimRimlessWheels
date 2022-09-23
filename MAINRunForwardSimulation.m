f481b497f276c16dc303d9f2f6305e9930c36ac3
Gharial-DTP <dpolet@rvc.ac.uk>
2022-09-23 12:52:18 +0100

    New Forward simulation function
    + can take in initial values
    + can fill in default options
    + script to run forward simulation

--- MAINRunForwardSimulation.m
+++ MAINRunForwardSimulation.m
@@ -2,6 +2,10 @@
 
 fName = '3DRimlessWheel.osim';
 
-options = struct('endTime',5,'stepSize',0.001,'reportInterval',0.01,'useVis',true);
+d2r = pi/180;
 
-RimlessWheelForwardSimulation(fName,options,'test');
+options = struct('endTime',3,'stepSize',0.001,'reportInterval',0.01,...
+    'useVis',true);
+initCoords = {'Pelvis_tx',5,10};
+
+simData = RimlessWheelForwardSimulation(fName,'test',initCoords,options);
--- RimlessWheelForwardSimulation.m
+++ RimlessWheelForwardSimulation.m
@@ -1,26 +1,58 @@
-function [simData,options] = RimlessWheelForwardSimulation(fName,options,saveName)
+function [simData,options] = RimlessWheelForwardSimulation(fName,saveName,initCoords,options)
+
+if nargin < 4
+    % initialize options as a blank struct. It will be filled with defaults
+    options = struct();
+    if nargin < 3
+        initCoords = [];
+        if nargin < 2 || isempty(saveName)
+            saveName = [strrep(fName,'.osim',''),'_',num2str(options.endTime),'s'];
+        end
+    end
+end
 
 % set default options
 fieldNames = fieldnames(options);
-neededFields = {'endTime','stepSize','integrator','accuracy','useVis','reportInterval'};
+reqFieldsAndDefaults = {'endTime',5,'stepSize',0.001,'integrator',1,...
+    'accuracy',0.001,'useVis',false,'reportInterval',0.01};
+reqFields = reqFieldsAndDefaults(1:2:end);
+reqDefaults = reqFieldsAndDefaults(2:2:end);
 
-if nargin < 3 || isempty(saveName)
-    saveName = [strrep(fName,'.osim',''),'_',num2str(options.endTime),'s'];
+for i = find(~ismember(reqFields,fieldNames))
+   options.(reqFields{i}) = reqDefaults{i};
 end
-
-
 
 import org.opensim.modeling.*
 
 osimModel = Model(fName);
 
-
 % Set the visualizer use.
 osimModel.setUseVisualizer(options.useVis);
+
+% set up a StatesTrajectoryReporter
+reporter = StatesTrajectoryReporter();
+reporter.setName('reporter');
+reporter.set_report_time_interval(options.reportInterval);
+osimModel.addComponent(reporter);
 
 % Initialize the underlying computational system and get a reference to
 % the system state.
 state = osimModel.initSystem();
+
+% initialize the coordinates
+if ~isempty(initCoords)
+    coordNames = initCoords(1:3:end);
+    coordValues = initCoords(2:3:end);
+    coordSpeeds = initCoords(3:3:end);
+    coordSet = osimModel.updCoordinateSet();
+    for i = 1:length(coordNames)
+        coordSet.get(coordNames{i}).setValue(state, coordValues{i});
+        coordSet.get(coordNames{i}).setSpeedValue(state, coordSpeeds{i});
+    end
+    
+end
+
+
 
 % Set the Vizualizer parameters
 if options.useVis
@@ -36,22 +68,16 @@ if options.useVis
 end
 
 
-
-
 %% Run a fwd simulation using the manager
 
 % set up a ForceReporter
 %forceReport = ForceReporter(osimModel);
 
-% set up a StatesTrajectoryReporter
-reporter = StatesTrajectoryReporter();
-reporter.setName('reporter');
-reporter.set_report_time_interval(options.reportInterval);
-osimModel.addComponent(reporter);
+
 
 % Initialize the underlying computational system and get a reference to
 % the system state.
-state = osimModel.initSystem();
+%state = osimModel.initSystem();
 
 htic = tic;
 disp('Running forward tool')
@@ -63,9 +89,8 @@ state = manager.integrate(options.endTime);
 htoc = toc(htic);
 
 fprintf('Forward Tool Finished in %.1f s\n',htoc);
-%% Get the states table from the manager and print the results.
-simData = struct;
-if saveName
+% Get the states table from the manager and print the results.
+
 %fTable = forceReport.getForcesTable(); % doesn't work...
 
 tic;
@@ -74,7 +99,8 @@ statesTraj = reporter.getStates();
 sTable = statesTraj.exportToTable(osimModel);
 fprintf('Took %.4f s\n',toc)
 
-
+%% Write results to .sto
+if saveName
 stofiles = STOFileAdapter();
 fprintf('Writing table to %s\n',[saveName,'.sto'])
 stofiles.write(sTable, [saveName,'.sto']);
@@ -82,8 +108,22 @@ stofiles.write(sTable, [saveName,'.sto']);
 % rewrite first line from "states" to something more informative
 newHeaderName = ['states',saveName];
 rewriteLine([saveName,'.sto'],newHeaderName,1);
+end
 
-%% Get the Data in matlab format
+%% Convert the Data to matlab format
+
+% first get table column labels; these will be in a format like
+% '/jointset/joint/coordname/value'
+% which is a bad format for osimTableToStruct and will generate pesky warnings.
+% The code below will convert the labels to 'coordname_value'
+nLabels = sTable.getNumColumns();
+for i = 0:nLabels-1
+    curLabel = char(sTable.getColumnLabel(i));
+    labelParts = strsplit(curLabel,'/');
+    newLabel = strjoin(labelParts(end-1:end),'_');
+    sTable.setColumnLabel(i,newLabel);
+end
+
 fprintf('Writing Table to Matlab Structure\n')
 tic;
 simData = osimTableToStruct(sTable);
@@ -105,11 +145,11 @@ end
 
 simData.SimulationTime = htoc;
 
-save([saveName,'.mat'],'simData')
-
-
-disp(['Output files written to ',saveName])
+if saveName
+    % same a .mat file
+    save([saveName,'.mat'],'simData')
+    disp(['Output files written to ',saveName])
 end
 
-end
+
 
